2d0807c9938d184956853cb915ddf42d79fea837
ntauler1 <34064823+ntauler1@users.noreply.github.com>
2018-07-23 17:23:08 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -151,3 +151,47 @@ function myFunction() {
     } 
   }
 }
+
+
+
+
+
+//list js
+
+/* List.js is required to make this table work. */
+
+var options = {
+  valueNames: [ { data: ['timestamp'] }, { data: ['status'] }, 'jSortNumber', 'jSortName', 'jSortTotal' ],
+  page: 6,
+  pagination: {
+    innerWindow: 1,
+    left: 0,
+    right: 0,
+    paginationClass: "pagination",
+    }
+};
+
+var tableList = new List('tableID', options);
+
+
+	$('.jPaginateNext').on('click', function(){
+	    var list = $('.pagination').find('li');
+	    $.each(list, function(position, element){
+	        if($(element).is('.active')){
+	            $(list[position+1]).trigger('click');
+	        }
+	    })
+	});
+
+
+	$('.jPaginateBack').on('click', function(){
+	    var list = $('.pagination').find('li');
+	    $.each(list, function(position, element){
+	        if($(element).is('.active')){
+	            $(list[position-1]).trigger('click');
+	        }
+	    })
+	});
+
+
+
