575b9a151dd5631e4408245b5ad727bf84678788
ntauler1 <34064823+ntauler1@users.noreply.github.com>
2018-07-23 08:33:20 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -114,3 +114,11 @@ angular.module("contactsApp", ['ngRoute'])
             Contacts.deleteContact(contactId);
         }
     });
+
+<script>
+// When the user clicks on <div>, open the popup
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+</script>
