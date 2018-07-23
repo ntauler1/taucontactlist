eadf9ced510091350b73894b2591f65d7069e18f
ntauler1 <34064823+ntauler1@users.noreply.github.com>
2018-07-23 08:35:21 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -115,10 +115,9 @@ angular.module("contactsApp", ['ngRoute'])
         }
     });
 
-<script>
+
 // When the user clicks on <div>, open the popup
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
-</script>
