58ae7c8b6aaeb5d3e34c1126462412e3740feda5
ntauler1 <34064823+ntauler1@users.noreply.github.com>
2018-07-23 15:56:42 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -171,7 +171,7 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
+var btnContainer = document.getElementById("contactId");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
