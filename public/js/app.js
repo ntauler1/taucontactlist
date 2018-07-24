6b5e593810d4052c57c22e243fad7f43d3f8f305
ntauler1 <34064823+ntauler1@users.noreply.github.com>
2018-07-23 23:13:31 -0400

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -134,9 +134,9 @@ function myFunction() {
 function myFunction() {
   // Declare variables 
   var input, filter, table, tr, td, i;
-  input = document.getElementById("myInput");
+  input = document.getElementById("contactInput");
   filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
+  table = document.getElementById("contactTable");
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
@@ -153,45 +153,6 @@ function myFunction() {
 }
 
 
-
-
-
-//list js
-
-/* List.js is required to make this table work. */
-
-var options = {
-  valueNames: [ { data: ['timestamp'] }, { data: ['status'] }, 'jSortNumber', 'jSortName', 'jSortTotal' ],
-  page: 6,
-  pagination: {
-    innerWindow: 1,
-    left: 0,
-    right: 0,
-    paginationClass: "pagination",
-    }
-};
-
-var tableList = new List('tableID', options);
-
-
-	$('.jPaginateNext').on('click', function(){
-	    var list = $('.pagination').find('li');
-	    $.each(list, function(position, element){
-	        if($(element).is('.active')){
-	            $(list[position+1]).trigger('click');
-	        }
-	    })
-	});
-
-
-	$('.jPaginateBack').on('click', function(){
-	    var list = $('.pagination').find('li');
-	    $.each(list, function(position, element){
-	        if($(element).is('.active')){
-	            $(list[position-1]).trigger('click');
-	        }
-	    })
-	});
 
 
 
